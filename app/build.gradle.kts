3e832c90fe6875de434ac942753fd39d819f23df
Pooja <poojakpeworld00@gmail.com>
2026-08-26 11:00:43 +0530

    v1.0.5: increment versionCode to 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         // shortcut + pinned-item APIs.
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
 
